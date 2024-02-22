aa7a5b8551af7edc09e75965c998b243d3281f2b
dlyzaren <66329596+dlyzaren@users.noreply.github.com>
2024-02-22 15:18:51 +0300

    Update AnaSayfa.java

--- Trendyolmilla/src/test/java/AnaSayfa.java
+++ Trendyolmilla/src/test/java/AnaSayfa.java
@@ -23,7 +23,15 @@ public class AnaSayfa extends Komutlar{
 
  public final String sepeteEklendi = ".add-to-basket-button-text-success";
 
+  public final String sepetim = "//p[.='Sepetim']";
 
+ public final String siparisiTamamla = ".CompleteOrder";
+
+ public final String sepetBilgeri = ".pb-summary-box";
+
+ public final String sepetiOnayla = "//div[@class='pb-summary']/div[4]/a[.='Sepeti Onayla']";
+
+ public final String gelenMesajim= ".pb-guest-order-flow-modal-content";
 
 
 
